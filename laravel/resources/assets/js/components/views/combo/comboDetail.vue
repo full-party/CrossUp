5452c0b1521dfd1ae3acbbf510f3a9183e003ea3
tiwu <theimaginationwasunbroken@gmail.com>
2017-06-04 16:17:11 +0900

    use combo detail api

--- laravel/resources/assets/js/components/views/combo/comboDetail.vue
+++ laravel/resources/assets/js/components/views/combo/comboDetail.vue
@@ -3,10 +3,16 @@
     <h1>Combo Detail</h1>
     <p>{{ $route.params.id }}</p>
     <p>
-      <router-link to="/combos">back combo list</router-link>
+      <p>Damege : {{combo.damage}}</p>
+      <p>Stun : {{combo.stun}}</p>
+      <p>Memo : {{combo.memo}}</p>
+      <p>Character : {{combo.character_id}}</p>
     </p>
     <p>
       <router-link :to="'/combos/' + $route.params.id + '/edit'">edit combo</router-link>
+    </p>
+    <p>
+      <router-link to="/combos">back combo list</router-link>
     </p>
   </section>
 </template>
@@ -16,3 +22,25 @@
     color: red;
   }
 </style>
+
+
+<script>
+  export default {
+    created() {
+        this.getCombo();
+      },
+    data() {
+      return {
+        combo: '',
+      }
+    },
+    methods: {
+      getCombo() {
+        axios.get('/api/combos/' + this.$route.params.id)
+        .then(res =>  {
+          this.combo = res.data;
+        })
+      },
+    }
+  }
+</script>
