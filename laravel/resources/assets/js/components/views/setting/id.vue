7969c34d43a74839530a56431d57f06ba1c5b348
丸山直樹 <andvari100@gmail.com>
2017-06-10 18:14:26 +0900

    make id setting

--- laravel/resources/assets/js/components/views/setting/id.vue
+++ laravel/resources/assets/js/components/views/setting/id.vue
@@ -1,12 +1,41 @@
 <template>
   <section>
     <h1>Setting Id</h1>
-    <router-link to="/setting">setting</router-link>
+    <input type="text" v-model="id" placeholder="User ID">
+    <p v-show="isEmpty">Please Input User ID</p>
+    <p class="error" v-show="!isEmpty && !idValidation">Error alphanumeric Only!</p>
+    <p v-show="idValidation">User ID OK!</p>
+    <router-link to="/setting">back</router-link>
+    <router-link to="/setting">
+      <button v-bind:disabled="isEmpty || !idValidation">
+        change
+      </button>
+    </router-link>
   </section>
 </template>
 
 <style scoped>
-  h1 {
+  h1, .error {
     color: red;
   }
 </style>
+
+<script>
+  var userIdRE = /^[a-zA-Z0-9]+$/;
+
+  export default {
+    data() {
+      return {
+        id: '',
+      }
+    },
+    computed: {
+      idValidation() {
+        return userIdRE.test(this.id);
+      },
+      isEmpty() {
+        return !this.id.trim();
+      }
+    }
+  };
+</script>
