be84f5399d7544a478429c724c05a9df4934b4ba
tiwu <theimaginationwasunbroken@gmail.com>
2017-06-13 00:29:36 +0900

    #46 make settig mailAddress parts

--- laravel/resources/assets/js/components/views/setting/address.vue
+++ laravel/resources/assets/js/components/views/setting/address.vue
@@ -1,7 +1,13 @@
 <template>
   <section>
     <h1>Setting Address</h1>
-    <router-link to="/setting">setting</router-link>
+    <form id="form">
+      <input type="email" v-model="email" placeholder="Email">
+      <p v-show="!emailValidation.validationRe">Please Input Email</p>
+      <p v-show="emailValidation.validationRe">Email OK!</p>
+      <router-link to="/setting">setting</router-link>
+      <input type="submit" value="Change" v-on:click.prevent="change">
+    </form>
   </section>
 </template>
 
@@ -10,3 +16,31 @@
     color: red;
   }
 </style>
+
+<script>
+  const emailRE = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  export default {
+    data() {
+      return {
+        email: '',
+      };
+    },
+    computed: {
+      emailValidation() {
+        return {
+          validationRe: emailRE.test(this.email),
+        };
+      },
+    },
+    methods: {
+      change() {
+        if (this.emailValidation.validationRe) {
+          console.log('true');
+        } else {
+          console.log('false');
+        }
+      },
+    },
+  }
+</script>
