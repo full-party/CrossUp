cd2943866e7a03af91c508eec67010a1f1b410c1
丸山直樹 <andvari100@gmail.com>
2017-10-28 15:25:35 +0900

    split moves to character

--- database/seeds/MovesTableSeeder.php
+++ database/seeds/MovesTableSeeder.php
@@ -16,20 +16,27 @@ class MovesTableSeeder extends Seeder
         DB::table('moves')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        $data = new SplFileObject('database/seeds/data/moves.csv');
-        $data->setFlags(SplFileObject::READ_CSV);
+        // キャラのCSVファイルを読み込む
+        foreach(glob('database/seeds/data/moves/{*.csv}',GLOB_BRACE) as $file){
+            $data = new SplFileObject($file);
+            $data->setFlags(SplFileObject::READ_CSV);
 
-        foreach ($data as $index => $line) {
-            // 1行目はヘッダー行なのでスキップ
-            if ($index > 0) {
-                $model = new Move();
-                $model->character_id = $line[0];
-                $model->id = $line[1];
-                $model->name = $line[2];
-                $model->damage = $line[3];
-                $model->stun = $line[4];
-                $model->meter = $line[5];
-                $model->save();
+            // キャラクターIDはファイル名から取得する
+            $fileName = $data->getBasename();
+            $characterId = explode('_', $fileName)[0];
+
+            foreach ($data as $index => $line) {
+                // 1行目はヘッダー行なのでスキップ
+                if ($index > 0) {
+                    $model = new Move();
+                    $model->character_id = $characterId;
+                    $model->id = $line[0];
+                    $model->name = $line[1];
+                    $model->damage = $line[2];
+                    $model->stun = $line[3];
+                    $model->meter = $line[4];
+                    $model->save();
+                }
             }
         }
     }
