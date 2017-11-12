84f3424ccf1005337db5be124539df564859fa40
tiwu <theimaginationwasunbroken@gmail.com>
2017-11-12 18:21:46 +0900

    #165 make combo input component

---
+++ resources/assets/js/components/common/combo-input.vue
@@ -0,0 +1,209 @@
+<template>
+  <form id="form">
+    <section class="combo__main-info">
+      <div @click="charaModal = true" class="combo__character">
+        <img src="/img/character.png" alt="character image">
+        <p class="combo__character__name">{{Combo.selectCharacterName}}</p>
+      </div>
+      <p class="combo__damage">
+        ダメージ : <input type="number" v-model="Combo.damage" placeholder="Input Total Damage">
+      </p>
+      <p class="combo__stun">
+        スタン : <input type="number" v-model="Combo.stun" placeholder="Input Total Stun">
+      </p>
+      <p class="combo__meter">
+        メーター : <input type="number" v-model="Combo.meter" placeholder="Input Total Meter">
+      </p>
+    </section>
+    <modal v-if="charaModal" @close="charaModalClose">
+      <div slot="modal-contents">
+        <p>Now Select : {{Combo.selectCharacterName}}</p>
+        <div v-for="character in characters">
+          <label v-bind:for="'character' + character.id">
+            <input type="radio" v-bind:id="'character' + character.id" v-bind:value="character.name" v-model="Combo.selectCharacterName">{{character.name}}
+          </label>
+        </div>
+        <button @click.prevent="charaModalClose">OK</button>
+      </div>
+    </modal>
+    <section class="combo__recipes">
+      <p>コンボ</p>
+      <ol>
+        <li v-for="move in Combo.combo">
+          {{move.name}}
+        </li>
+      </ol>
+    </section>
+    <section class="combo__move__input">
+      <div class="combo__move__input__header">
+        <span class="combo__move__input__text">コンボ入力</span>
+        <div class="combo__move__input__menu">
+          <button @click.prevent="oneDelete" class="combo__move__input__one-delete">1つ削除</button>
+          <button @click.prevent="allDelete" class="combo__move__input__all-delete">全て削除</button>
+        </div>
+      </div>
+      <div class="moveList">
+        <div v-for="move in moves" class="move" :move-id="move.id" @click="setMove(move)">
+          <span class="move__text">{{move.name}}</span>
+        </div>
+      </div>
+    </section>
+    <section class="combo__memo">
+      <p>メモ</p>
+      <textarea v-model="Combo.memo" placeholder="Input Memo"></textarea>
+    </section>
+  </form>
+</template>
+
+<style scoped>
+  p {
+    margin: 0;
+  }
+  .combo__main-info {
+    display: grid;
+    grid-template-columns: 80px 1fr;
+    grid-template-rows: repeat(3, 1fr);
+    border: 1px solid #D0D0D0;
+    background: #fff;
+    padding: 10px;
+    margin-bottom: 15px;
+  }
+  .combo__character {
+    grid-row: 1 / 4;
+    grid-column: 1 / 2;
+    padding-right: 15px;
+    text-align: center;
+  }
+  .combo__damage {
+    grid-row: 1 / 2;
+    grid-column: 2 / 3;
+  }
+  .combo__stun {
+    grid-row: 2 / 3;
+    grid-column: 2 / 3;
+  }
+  .combo__meter {
+    grid-row: 3 / 4;
+    grid-column: 2 / 3;
+  }
+  .combo__character__name {
+    padding: 10px 0;
+  }
+  ol {
+    list-style: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+  }
+  li + li:before{
+      content: " > ";
+      padding: 0 10px;
+  }
+  .combo__move__input, .combo__recipes, .combo__memo {
+    border: 1px solid #D0D0D0;
+    background: #fff;
+    padding: 10px;
+    margin-bottom: 15px;
+  }
+  .combo__move__input__header {
+    padding-bottom: 10px;
+  }
+  .combo__move__input__header:after {
+    content:" ";
+    display:block;
+    clear:both;
+  }
+  .combo__move__input__text {
+    float: left;
+  }
+  .combo__move__input__menu {
+    display: inline-block;
+    float: right;
+  }
+  .combo__move__input__one-delete, .combo__move__input__all-delete {
+    background: #E6E6E6;
+    border: none;
+    height: 26px;
+    padding: 0;
+    width: 96px;
+    margin: auto;
+  }
+  .move {
+    display: inline-block;
+    padding: 7px 5px;
+  }
+  .move__text {
+    background-color: #00AC0D;
+    padding: 5px 11px;
+  }
+  textarea {
+    width: 100%;
+    border: 1px solid black;
+  }
+</style>
+
+<script>
+  export default {
+    components: {
+      modal: require('./modal.vue'),
+    },
+    props: {
+      Combo: {
+        type: Object,
+        required: true
+      },
+    },
+    created() {
+      this.getCharacters();
+    },
+    data() {
+      return {
+        characters: [],
+        charaModal: false,
+        moves: []
+      }
+    },
+    methods: {
+      getCharacters() {
+        axios.get('/api/characters',{
+          params: {
+            gameId: this.$store.state.game.id,
+          }
+        })
+        .then(res => {
+          this.characters = res.data.data;
+        });
+      },
+      charaModalClose() {
+        this.charaModal = false;
+        this.setCharacter();
+      },
+      setCharacter() {
+        for(let id in this.characters) {
+          if(this.Combo.selectCharacterName === this.characters[id].name) {
+            this.Combo.selectCharacterId = this.characters[id].id;
+            break;
+          }
+        }
+        axios.get('/api/moves', {
+          params: {
+            characterId: this.Combo.selectCharacterId,
+          }
+        })
+        .then(res =>  {
+          this.moves = res.data;
+        });
+      },
+      setMove(move) {
+        this.Combo.combo.push(move);
+      },
+      allDelete() {
+        this.Combo.combo = [];
+      },
+      oneDelete() {
+        this.Combo.combo.pop();
+      },
+    }
+  }
+</script>
--- resources/assets/js/components/views/combo/comboEdit.vue
+++ resources/assets/js/components/views/combo/comboEdit.vue
@@ -1,30 +1,8 @@
 <template>
   <section>
     <h1>Combo Edit</h1>
-    <p>{{ $route.params.id }}</p>
-    <p>
-      <p>Character : {{combo.character.name}}</p>
-      <h3>Input Combo</h3>
-      <span v-for="move in combo.combo">
-        {{move.name}}
-      </span>
-      </div>
-      <div class="moveList">
-        <div v-for="move in moves" class="move" :move-id="move.id" @click="setMove(move)">
-          {{move.name}}
-        </div>
-      </div>
-      <button @click.prevent="allDelete">All Delete</button>
-      <button @click.prevent="oneDelete">One Delete</button>
-      <h3>Input Total Damage</h3>
-      <input type="number" v-model="combo.damage" placeholder="Input Total Damage">
-      <h3>Input Total Stun</h3>
-      <input type="number" v-model="combo.stun" placeholder="Input Total Stun">
-      <p>Meter : {{combo.meter}}</p>
-      <h3>Input Memo</h3>
-      <textarea v-model="combo.memo" placeholder="Input Memo"></textarea>
-    </p>
-    <p @click="updateCombo" v-if="combo.myComboFlg">update</p>
+    <comboInput :Combo="Combo"></comboInput>
+    <p @click="updateCombo" v-if="Combo.myComboFlg">update</p>
     <router-link :to="'/combos/' + $route.params.id">back combo detail</router-link>
   </section>
 </template>
@@ -33,34 +11,20 @@
   h1 {
     color: red;
   }
-
-  textarea {
-    width: 100%;
-  }
-
-  .moveList {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(80px, 1fr));
-    grid-column-gap: 10px;
-    grid-row-gap: 10px;
-  }
-
-  .move {
-    background-color: #00AC0D;
-    text-align: center;
-  }
 </style>
 
 
 <script>
   export default {
+    components: {
+      comboInput: require('../../common/combo-input.vue'),
+    },
     created() {
         this.getCombo();
       },
     data() {
       return {
-        moves: [],
-        combo: {
+        Combo: {
           character: '',
         },
       }
@@ -69,11 +33,11 @@
       getCombo() {
         axios.get('/api/combos/' + this.$route.params.id)
         .then(res =>  {
-          this.combo = res.data;
+          this.Combo = res.data;
           // 描画後に追加されるコンボ情報をVueで追跡させる
-          this.$set(this.combo, 'combo', []);
-          for(let recipe of this.combo.recipes) {
-            this.combo.combo.push(recipe.move);
+          this.$set(this.Combo, 'combo', []);
+          for(let recipe of this.Combo.recipes) {
+            this.Combo.combo.push(recipe.move);
           }
           this.getMove();
         })
@@ -81,7 +45,7 @@
       getMove() {
         axios.get('/api/moves', {
           params: {
-            characterId: this.combo.character_id,
+            characterId: this.Combo.character_id,
           }
         })
         .then(res =>  {
@@ -89,20 +53,11 @@
         });
       },
       updateCombo() {
-        console.log(this.combo);
-        axios.put('/api/combos/' + this.combo.id, this.combo)
+        console.log(this.Combo);
+        axios.put('/api/combos/' + this.Combo.id, this.Combo)
         .then(res => {
           console.log(res);
         });
-      },
-      setMove(move) {
-        this.combo.combo.push(move);
-      },
-      allDelete() {
-        this.combo.combo = [];
-      },
-      oneDelete() {
-        this.combo.combo.pop();
       },
     }
   }
