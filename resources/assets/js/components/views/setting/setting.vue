b35026cb3b2f60431ae9b257c40bbf042a4c70d4
tiwu <theimaginationwasunbroken@gmail.com>
2017-11-18 19:02:41 +0900

    #177 design setting

--- resources/assets/js/components/views/setting/setting.vue
+++ resources/assets/js/components/views/setting/setting.vue
@@ -1,26 +1,50 @@
 <template>
   <section>
-    <h1>Setting</h1>
-    <p>
-      <router-link to="/setting/id">setting id</router-link>
-    </p>
-    <p>
-      <router-link to="/setting/address">setting address</router-link>
-    </p>
-    <p>
-      <router-link to="/setting/password">setting password</router-link>
-    </p>
-    <p>
-      <router-link to="/setting/delete/id">delete id</router-link>
-    </p>
-    <p>
-      <router-link to="/combos">back combo list</router-link>
-    </p>
+    <h1>設定</h1>
+    <div class="setting__menu">
+      <router-link to="/setting/id" class="setting__menu__item">ID変更</router-link>
+      <router-link to="/setting/address" class="setting__menu__item">メールアドレス変更</router-link>
+      <router-link to="/setting/password" class="setting__menu__item">パスワード変更</router-link>
+      <router-link to="/setting/delete/id" class="setting__menu__item">退会</router-link>
+    </div>
+    <section class="menu">
+      <router-link class="back-link" to="/combos">
+        <img src="/img/left-arrow.png" alt="left arrow">
+        戻る
+      </router-link>
+    </section>
   </section>
 </template>
 
 <style scoped>
   h1 {
-    color: red;
+    padding: 0 0 10px 10px;
+  }
+  .setting__menu {
+    background-color: #fff;
+    padding-left: 30px;
+    margin-bottom: 30px;
+  }
+  .setting__menu__item {
+    border-bottom: 1px solid #DFDFDF;
+    height: 40px;
+    line-height: 40px;
+    display: block;
+    color: #4A4A4A;
+  }
+  .setting__menu__item:last-child {
+    border-bottom: none;
+  }
+  .menu {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    background: #fff;
+    text-align: center;
+    line-height: 40px;
+  }
+  .back-link {
+    grid-row: 1 / 2;
+    grid-column: 1 / 2;
   }
 </style>
